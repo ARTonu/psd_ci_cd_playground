f6773a3cba6bec955e69068676f9c2f54a254e86
Tonu <anisur.rahman@bjitgroup.com>
2024-08-30 09:29:23 +0600

    Initialize Spring boot project

---
+++ src/test/java/com/psd/cicd/play/PlayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psd.cicd.play;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
